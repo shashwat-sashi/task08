a551b67f60fa92f808890b5b8ac95f66b0231526
shashwat-sashi <shashwat_shandilya@epam.com>
2025-05-31 13:38:14 +0530

    Update locals.tf

--- task08/locals.tf
+++ task08/locals.tf
@@ -17,7 +17,7 @@ locals {
   redis_primary_key_secret_name = "redis-primary-key"
 
   tags = {
-    Creator = "sshashwat_shandilya@epam.com"
+    Creator = "shashwat_shandilya@epam.com"
   }
 
   dns_name_label = "mydnslabel"
